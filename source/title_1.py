1915edf25efebabe3743f57f3a2db9945ee7923f
danishdeepsphere <120715128+danishdeepsphere@users.noreply.github.com>
2023-02-23 16:08:55 +0530

    Add files via upload

---
+++ source/title_1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+def title():
+    st.markdown("<p style='text-align: center; color: blue; font-size:29px;'>Simple application built on Streamlit</p>", unsafe_allow_html=True)
+    
