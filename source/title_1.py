17db596d35a790fd59c5d959cc16160e74ad0692
VikramanDeepsphere <121026513+VikramanDeepsphere@users.noreply.github.com>
2023-03-08 18:37:35 +0530

    Add files via upload

---
+++ source/title_1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+def title():
+    st.markdown("<p style='text-align: center; color: blue; font-size:20px;'>ChatGPT Application for Question generations and Content generation</p>", unsafe_allow_html=True)
+    
