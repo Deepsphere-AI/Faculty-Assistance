26e75c76a53f297b36eca8351d32863c00e0f4ea
danishdeepsphere <120715128+danishdeepsphere@users.noreply.github.com>
2023-03-16 18:55:13 +0530

    Delete title_1.py

--- source/title_1.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from streamlit_option_menu import option_menu
-
-def title():
-    st.markdown("<p style='text-align: center; color: blue; font-size:20px;'>ChatGPT Application for Question generations and Content generation</p>", unsafe_allow_html=True)
-    
