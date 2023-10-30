fca9bb7a128c3b1027f72742585a60a470b639e3
danishdeepsphere <120715128+danishdeepsphere@users.noreply.github.com>
2023-10-30 17:35:28 +0530

    Add files via upload

--- source/title_1.py
+++ source/title_1.py
@@ -2,6 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 def title():
-    st.markdown("<p style='text-align: center; color: blue; font-size:20px;'>ChatGPT Converts Next-Gen Education Vision into Action </p>", unsafe_allow_html=True)
+    st.write("## ")
+    st.markdown("<p style='text-align: center; color: blue; font-size:20px;'>With GEN AI, next-generation education visions become reality </p>", unsafe_allow_html=True)
     st.markdown("### ")
     
